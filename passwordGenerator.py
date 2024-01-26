0f42170b292e2b540323464eb74552a961e8efd7
Jean-Luc Felix <jlfelix522@yahoo.com>
2024-01-25 20:06:21 -0800

    printed string library

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,1 +1,18 @@
+import random
+import string
+
 print("Hello from Password Generator!")
+
+plength = input("Enter password length: ")
+
+print(plength)
+
+lowercaseLetters = string.ascii_lowercase
+uppercaseLetters = string.ascii_uppercase
+numbers = string.digits
+symbols = string.punctuation
+
+print(lowercaseLetters)
+print(uppercaseLetters)
+print(numbers)
+print(symbols)
