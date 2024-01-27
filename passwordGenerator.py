d044e8a11aaebdd952b458ed5523eec9f030eddc
Jean-Luc Felix <jlfelix522@yahoo.com>
2024-01-26 20:45:23 -0800

    Renamed file

---
+++ passwordGenerator.py
@@ -0,0 +1,29 @@
+import random
+import string
+
+print("Hello from Password Generator!")
+
+plength = int(input("Enter password length: "))
+
+lowercaseLetters = string.ascii_lowercase
+uppercaseLetters = string.ascii_uppercase
+numbers = string.digits
+symbols = string.punctuation
+
+keyboardCommands = []
+
+keyboardCommands.extend(list(lowercaseLetters))
+keyboardCommands.extend(list(uppercaseLetters))
+keyboardCommands.extend(list(numbers))
+keyboardCommands.extend(list(symbols))
+
+generatedPassword = ''
+
+while True:
+    randomInput = random.choice(keyboardCommands)
+    generatedPassword += randomInput
+    plength -= 1
+    if plength == 0:
+        break
+
+print("Your new password is: " + generatedPassword)
