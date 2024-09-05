d2034fc851942bb24bc5063a5b1e009e3f15165b
Jean-Luc Felix <jlfelix522@yahoo.com>
2024-09-05 02:27:33 -0700

    Removed extra code

--- passwordEncryptor.py
+++ passwordEncryptor.py
@@ -1,12 +1,5 @@
 from cryptography.fernet import Fernet
 
-# inputMessage = input("Input your message here: ")
-
-# key = Fernet.generate_key()
-# fernet = Fernet(key)
-
-# encryptMessage = fernet.encrypt(bytes(inputMessage, encoding="utf8"))
-
 def passwordEncrypt():
     inputPassword = input("Input your message here: ")
     key = Fernet.generate_key()
