44424001a06472f2b019a39e6ea02b78d65dca4c
Jean-Luc Felix <jlfelix522@yahoo.com>
2024-09-05 02:21:12 -0700

    Refactored code into function

--- passwordEncryptor.py
+++ passwordEncryptor.py
@@ -1,11 +1,22 @@
 from cryptography.fernet import Fernet
 
-inputMessage = input("Input your message here: ")
+# inputMessage = input("Input your message here: ")
 
-key = Fernet.generate_key()
-fernet = Fernet(key)
+# key = Fernet.generate_key()
+# fernet = Fernet(key)
 
-encryptMessage = fernet.encrypt(bytes(inputMessage, encoding="utf8"))
+# encryptMessage = fernet.encrypt(bytes(inputMessage, encoding="utf8"))
 
-print("Here is your encrypted password")
-print(encryptMessage)
+# print("Here is your encrypted password")
+# print(encryptMessage)
+
+def passwordEncrypt(inputPassword):
+    inputPassword = input("Input your message here: ")
+    key = Fernet.generate_key()
+    fernet = Fernet(key)
+    encryptMessage = fernet.encrypt(bytes(inputPassword, encoding="utf8"))
+
+    print("Here is your encrypted password")
+    return encryptMessage
+
+print(passwordEncrypt("password"))
