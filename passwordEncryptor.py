0cf370a681b3659e65a2c8d4db84defc22f3eccd
Jean-Luc Felix <jlfelix522@yahoo.com>
2024-09-05 18:32:48 -0700

    Added note

--- passwordEncryptor.py
+++ passwordEncryptor.py
@@ -4,6 +4,7 @@ def passwordEncrypt():
     inputPassword = input("Input your message here: ")
     key = Fernet.generate_key()
     fernet = Fernet(key)
+    # convert string to bytes
     encryptMessage = fernet.encrypt(bytes(inputPassword, encoding="utf8"))
 
     print("Here is your encrypted password: ")
