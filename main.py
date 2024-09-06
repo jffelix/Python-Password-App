2beeecc6bd2cd74ec2ae3e6cfc8cf32ed4cd31d3
Jean-Luc Felix <jlfelix522@yahoo.com>
2024-09-06 01:40:14 -0700

    Added conditionals to main

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 print("Welcome to Password Generator!")
+
+userChoice = input("Choose from the following: Generator, Encryptor, Decryptor ")
+
+if (userChoice.lower() == "generator"):
+    print("You chose Generator!")
+elif (userChoice.lower() == "encryptor"):
+    print("You chose Encryptor!")
+elif (userChoice.lower() == "decryptor"):
+    print('You chose Decryptor!')
+else:
+    print("Error, choose again.")
